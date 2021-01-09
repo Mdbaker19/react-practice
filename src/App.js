ee22ea579fa53f41d7e9effca9e6cf0a21dfb9ff
Matthew <matthew.d.baker20@gmail.com>
2021-01-09 16:30:36 -0600

    restructure code to app component(?)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
-class App extends Component {
-    state = {
+const App = (props) => {
+
+    const [ people, setPeople ] = useState ({
         persons: [
             { name: 'matt', age: 25, bday: 4 },
             { name: 'man', age: 22, bday: 1 },
             { name: 'nemo', age: 2, bday: 3 },
-        ]
-    }
+        ],
+        otherState: "set people value"
+    });
 
-    changeNameHandler = () => {
-        this.setState({
+    const [otherState, setOtherState] = useState("some other value");
+
+    console.log(people, otherState);
+
+    // when this is called, otherState is basically removed, needs to be added as this just overwrites people (the persons array)
+    const changeNameHandler = () => {
+        setPeople({
             persons: [
-                { name: 'Mattthew', age: 25, bday: 4 },
-                { name: 'Hue Man', age: 22, bday: 1 },
-                { name: 'Nemo the Dog', age: 2, bday: 3 },
+                { name: 'Mattthew', age: 25, bday: "4th" },
+                { name: 'Hue Man', age: 22, bday: "1st" },
+                { name: 'Nemo the Dog', age: 2, bday: "3rd" },
             ]
         });
     }
-
-    render () {
-        // jsx syntax
-        return (
-            <div className="main">
-                <div id="buttonArea">
-                    <button onClick={this.changeNameHandler}>Proper names</button>
-                </div>
-                <Person day={this.state.persons[0].bday} name={this.state.persons[0].name} age={this.state.persons[0].age}>coding, learning, cars, building and music</Person>
-                <Person day={this.state.persons[1].bday} name={this.state.persons[1].name} age={this.state.persons[1].age}>being a human, a man and all things people</Person>
-                <Person day={this.state.persons[2].bday} name={this.state.persons[2].name} age={this.state.persons[2].age}>play, speak, tricks and treats</Person>
+    // jsx syntax
+    return (
+        <div className="main">
+            <div id="buttonArea">
+                <button onClick={changeNameHandler}>Proper names</button>
             </div>
-        );
-    }
+            <Person day={people.persons[0].bday} name={people.persons[0].name} age={people.persons[0].age}>coding, learning, cars, building and music</Person>
+            <Person day={people.persons[1].bday} name={people.persons[1].name} age={people.persons[1].age}>being a human, a man and all things people</Person>
+            <Person day={people.persons[2].bday} name={people.persons[2].name} age={people.persons[2].age}>play, speak, tricks and treats</Person>
+        </div>
+    );
 }
 
 export default App;
+
+
