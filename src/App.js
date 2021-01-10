bc72ca169fe246eeb714ab57083ca1702d0c0d95
Matthew <matthew.d.baker20@gmail.com>
2021-01-10 10:42:23 -0600

    added input text change, formatted code, click events to card names

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = (props) => {
             { name: 'man', age: 22, bday: 1 },
             { name: 'nemo', age: 2, bday: 3 },
         ],
-        otherState: "set people value"
+        otherState: "set people value for the set people called in name handlers"
     });
 
     const [otherState, setOtherState] = useState("some other value");
@@ -18,24 +18,48 @@ const App = (props) => {
     console.log(people, otherState);
 
     // when this is called, otherState is basically removed, needs to be added as this just overwrites people (the persons array)
-    const changeNameHandler = () => {
+    const changeNameHandler = (nameList, name) => {
         setPeople({
             persons: [
-                { name: 'Mattthew', age: 25, bday: "4th" },
-                { name: 'Hue Man', age: 22, bday: "1st" },
-                { name: 'Nemo the Dog', age: 2, bday: "3rd" },
+                { name: name ?? nameList[0], age: 25, bday: "4th" },
+                { name: name ?? nameList[1], age: 22, bday: "1st" },
+                { name: name ?? nameList[2], age: 2, bday: "3rd" },
             ]
         });
     }
+
+    const properNames = ["Matthew", "Hue Man", "Nemo Goat Snake"];
+
+    // this e object is passed automatically by React
+    const nameChangeHandler = (e) => {
+        setPeople ( {
+            persons : [
+                { name: "M", age: 1 },
+                { name: e.target.value, age: 1 },
+                { name: "N", age: 1 },
+            ]
+        })
+    }
+
     // jsx syntax
     return (
         <div className="main">
             <div id="buttonArea">
-                <button onClick={changeNameHandler}>Proper names</button>
+                <button onClick={ () => changeNameHandler(properNames, null) }>Proper names</button>
             </div>
-            <Person day={people.persons[0].bday} name={people.persons[0].name} age={people.persons[0].age}>coding, learning, cars, building and music</Person>
-            <Person day={people.persons[1].bday} name={people.persons[1].name} age={people.persons[1].age}>being a human, a man and all things people</Person>
-            <Person day={people.persons[2].bday} name={people.persons[2].name} age={people.persons[2].age}>play, speak, tricks and treats</Person>
+            <Person click={ changeNameHandler.bind(this, null, "Matthew B!")}
+                    day={people.persons[0].bday} name={people.persons[0].name}
+                    age={people.persons[0].age}>coding, learning, cars, building and music</Person>
+
+            <Person click={ () => changeNameHandler(null, "Hue Man!")}
+                    changed={ nameChangeHandler }
+                    day={people.persons[1].bday} name={people.persons[1].name}
+                    age={people.persons[1].age}>being a human, a man and all things people</Person>
+
+            <Person click={ () => changeNameHandler(null, "Nemo Goat Snake!")}
+                    day={people.persons[2].bday} name={people.persons[2].name}
+                    age={people.persons[2].age}>play, speak, tricks and treats</Person>
+
         </div>
     );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,10 +4,11 @@ const person = (props) => {
     return (
         <div className="personCard">
             <p>My birth day is on the {props.day} day of the month</p>
-            <p>My name is {props.name}</p>
+            <p onClick={props.click}>My name is {props.name}</p>
             <p>My age is {props.age}</p>
             {/*children refers to the html in between the tags, empty tag if no info passed in tags*/}
             <p>My hobbies are: {props.children}</p>
+            <input type="text" onChange={props.changed}/>
         </div>
     )
 }
